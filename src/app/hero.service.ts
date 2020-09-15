0bca8a94258f8a1702a5e454e9346bed58440300
roach <tmdgusya@naver.com>
2020-09-15 14:06:11 +0900

    menu 구현부분 코드 개선

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -28,8 +28,8 @@ export class HeroService {
   makeContext(){ // 메뉴 생성하는 곳
     const menu: IMenuData[] = [
       {title: "영웅 추가", disable: true, action: () => {console.log("영웅 추가")}},
-      {title: "영웅 수정", disable: false, action: () => {console.log("영웅 수장")}},
-      {title: "영웅 삭제", disable: true, action: () => {console.log("영웅 삭제")}}
+      {title: "영웅 수정", disable: false, action: () => {console.log("영웅 수장")},  menu: [{title: "submenu", disable: true}]},
+      {title: "영웅 삭제", disable: true, action: () => {console.log("영웅 삭제")}, menu: [{title: "submenu", disable: true}]}
     ]
     return menu;
   }
