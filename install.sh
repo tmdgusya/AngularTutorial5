07accc893ddebb6efa5e3774fc32e5a0d2e47d6f
roach <tmdgusya@naver.com>
2020-09-17 14:31:13 +0900

    shell 을 통한 자동화 의존성 설치 가능

---
+++ install.sh
@@ -0,0 +1,9 @@
+npm install -g typescript
+npm install -g @angular/cli@latest
+npm install -g @angular/core
+npm install -g tslib
+npm install -g ngx-contextmenu
+npm install -g @angular-devkil/build-angular
+npm install -g angular2-contextmenu
+ng update
+echo update 목록뜨면 ng update 해당꺼를 통해 초기화 시켜주면됨.
